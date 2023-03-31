cb0539df005e15877b0a269a0383f3368208fb13
Jinwoo Park <zvzv9808@gmail.com>
2023-03-31 20:58:37 +0900

    feat: createBoard Service & Controller

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Board } from './boards.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
@@ -8,5 +9,13 @@ export class BoardsController {
   @Get()
   getAllBoards() {
     return this.boardsService.getAllBoards();
+  }
+
+  @Post('/')
+  createBoard(
+    @Body('title')title: string,
+    @Body('description')description: string
+  ): Board {
+    return this.boardsService.createBoard(title, description);
   }
 }
