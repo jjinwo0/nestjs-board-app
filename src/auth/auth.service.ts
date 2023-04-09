9624168b60ef31b78b1a51eba2501c1ea3106bca
Jinwoo Park <zvzv9808@gmail.com>
2023-04-09 22:04:39 +0900

    7-6) Use Bcrypt for Secret Password with Salt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { User } from './user.entity';
 
 @Injectable()
 export class AuthService {
